b6b90be1646c61eb6373d6a445d084826e97fc97
VuTrungNghia <vunghia467@gmail.com>
2023-07-19 19:02:53 +0700

    up

--- src/main/java/com/example/DoAnJava/controller/HomeController.java
+++ src/main/java/com/example/DoAnJava/controller/HomeController.java
@@ -10,12 +10,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RestController
 public class HomeController {
     @Autowired
     private RestTemplate restTemplate;
