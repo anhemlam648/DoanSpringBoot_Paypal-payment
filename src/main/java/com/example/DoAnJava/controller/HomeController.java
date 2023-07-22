f689770b463ef1904238805a59a05fd2ee98fe7d
VuTrungNghia <vunghia467@gmail.com>
2023-07-22 13:00:36 +0700

    update

--- src/main/java/com/example/DoAnJava/controller/HomeController.java
+++ src/main/java/com/example/DoAnJava/controller/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
     @Autowired
     private RestTemplate restTemplate;
