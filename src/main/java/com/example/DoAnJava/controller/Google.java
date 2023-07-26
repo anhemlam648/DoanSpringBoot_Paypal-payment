af207a2c30caebf09a1bce8ec0f4c0c4b7075366
VuTrungNghia <vunghia467@gmail.com>
2023-07-26 13:05:16 +0700

    update

--- src/main/java/com/example/DoAnJava/controller/Google.java
+++ src/main/java/com/example/DoAnJava/controller/Google.java
@@ -1,19 +1,19 @@
-package com.example.DoAnJava.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.security.Principal;
-
-@Controller
-@RequestMapping("/google")
-public class Google {
-
-    @GetMapping("/user")
-    public Principal user(Principal principal){
-        System.out.println("username : " + principal.getName());
-        return principal;
-    }
-}
+//package com.example.DoAnJava.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//import java.security.Principal;
+//
+//@Controller
+//@RequestMapping("/google")
+//public class Google {
+//
+//    @GetMapping("/user")
+//    public Principal user(Principal principal){
+//        System.out.println("username : " + principal.getName());
+//        return principal;
+//    }
+//}
