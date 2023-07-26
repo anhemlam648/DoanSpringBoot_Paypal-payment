205ceee676950e76050322a6372c411b84ef8d47
VuTrungNghia <vunghia467@gmail.com>
2023-07-26 22:54:06 +0700

    update

--- src/main/java/com/example/DoAnJava/controller/Google.java
+++ src/main/java/com/example/DoAnJava/controller/Google.java
@@ -1,5 +1,7 @@
 package com.example.DoAnJava.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.security.Principal;
 
 @Controller
-@RequestMapping("/google")
 public class Google {
 
-    @GetMapping("/user")
-    public Principal user(Principal principal){
-        System.out.println("username : " + principal.getName());
-        return principal;
+    @GetMapping("/oauth2/callback/google")
+    public String oauth2LoginCallback(@AuthenticationPrincipal OAuth2User principal) {
+        return "redirect:/";
     }
 }
+
