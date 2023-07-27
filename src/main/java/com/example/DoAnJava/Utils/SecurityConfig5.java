facdefe03d0a513b881b961eac6e1928273eb40d
VuTrungNghia <vunghia467@gmail.com>
2023-07-27 12:36:02 +0700

    update

--- src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
+++ src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
@@ -1,23 +1,22 @@
-//package com.example.DoAnJava.Utils;
-////
-////
-////import org.springframework.context.annotation.Bean;
-////import org.springframework.context.annotation.Configuration;
-////import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-////import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-////import org.springframework.security.core.parameters.P;
-////import org.springframework.security.web.SecurityFilterChain;
-////
-////@Configuration
-////@EnableWebSecurity
-////public class SecurityConfig5 {
-////
-////
-////    @Bean
-////    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-////
-////       http.authorizeHttpRequests().anyRequest().authenticated().and().oauth2Login();
-////       return  http.build();
-////    }
-////
-////}
+package com.example.DoAnJava.Utils;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig5 {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request->request
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login();
+        return http.build();
+    }
+}
