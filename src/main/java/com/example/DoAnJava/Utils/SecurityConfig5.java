cf13da6078f16e98e6fab66f5bf5138257791191
VuTrungNghia <vunghia467@gmail.com>
2023-07-27 17:32:36 +0700

    update

--- src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
+++ src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
@@ -15,7 +15,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig5 {
-    @Bean
+
+        @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailService();
     }
@@ -33,59 +34,22 @@ public class SecurityConfig5 {
 
         return auth;
     }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//    return     http.authorizeHttpRequests(request->request
-//                        .anyRequest()
-//                        .authenticated())
-//                .oauth2Login(oauth2Login ->
-//                        oauth2Login.loginPage("/login") // Specify your custom login page here
-//                                .defaultSuccessUrl("/") // Change to your desired landing page after successful login
-        @Bean
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .csrf(csrf -> csrf.disable())
-                    .authorizeHttpRequests(auth -> auth
-                            .requestMatchers("/js/**", "/", "/register", "/404")
-                            .permitAll()
-                            .requestMatchers("/admin/product")
-                            .hasAnyAuthority("ADMIN")
-                            .requestMatchers("/product/list")
-                            .hasAnyAuthority("ADMIN", "USER")
-                            .anyRequest().authenticated()
-                    )
-                    .logout(logout -> logout.logoutUrl("/logout")
-                            .logoutSuccessUrl("/login")
-                            .deleteCookies("JSESSIONID")
-                            .invalidateHttpSession(true)
-                            .clearAuthentication(true)
-                            .permitAll()
-                    )
-                    .formLogin(formLogin -> formLogin.loginPage("/login")
-                            .loginProcessingUrl("/login")
-                            .defaultSuccessUrl("/")
-                            .permitAll()
-                    )
-                    .rememberMe(rememberMe -> rememberMe.key("uniqueAndSecret")
-                            .tokenValiditySeconds(86400)
-                            .userDetailsService(userDetailsService())
-                    )
-                    .exceptionHandling(exceptionHandling ->
-                            exceptionHandling.accessDeniedPage("/404")
-                    )
-                    .oauth2Login(oauth2Login ->
-                            oauth2Login.loginPage("/login") // Specify your custom login page here
-                                    .defaultSuccessUrl("/") // Change to your desired landing page after successful login
-                    );
-        }
-
-        // Define the SecurityFilterChain to enable both OAuth2 and regular form login
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                    .oauth2Login();
-            return http.build();
-        }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+     return    http.authorizeHttpRequests(request->request
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login(
+                        oauth2Login ->
+                        oauth2Login.loginPage("/login") // Specify your custom login page here
+                                .defaultSuccessUrl("/")
+                )
+                        .rememberMe(rememberMe -> rememberMe.key("uniqueAndSecret")
+                        .tokenValiditySeconds(86400)
+                        .userDetailsService(userDetailsService())
+                )
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.accessDeniedPage("/404"))
+                .build();
+    }
 }
