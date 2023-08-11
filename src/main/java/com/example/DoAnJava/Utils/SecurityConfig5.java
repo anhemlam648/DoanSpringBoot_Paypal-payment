b923c6d47c914ec0e8529cc261531483d385ecea
VuTrungNghia <vunghia467@gmail.com>
2023-08-11 12:05:49 +0700

    update

--- src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
+++ src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
@@ -1,30 +1,30 @@
-package com.example.DoAnJava.Utils;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig5{
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest()
-                                .authenticated()
-                )
-                .oauth2Login();
-
-        return http.build();
-    }
-
-}
+//package com.example.DoAnJava.Utils;
+//
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig5{
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeRequests(authorizeRequests ->
+//                        authorizeRequests
+//                                .anyRequest()
+//                                .authenticated()
+//                )
+//                .oauth2Login();
+//
+//        return http.build();
+//    }
+//
+//}
