fb4c857d61fe9904bcd56bc6f10520fcf34d8a61
VuTrungNghia <vunghia467@gmail.com>
2023-08-11 12:02:34 +0700

    update

--- src/main/java/com/example/DoAnJava/Utils/CustomOAuth2UserService.java
+++ src/main/java/com/example/DoAnJava/Utils/CustomOAuth2UserService.java
@@ -1,22 +1,22 @@
-package com.example.DoAnJava.Utils;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.Map;
-
-@Service
-public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User user =  super.loadUser(userRequest);
-        return new CustomOAuth2User(user);
-    }
-}
+//package com.example.DoAnJava.Utils;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+//import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+//import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Collection;
+//import java.util.Map;
+//
+//@Service
+//public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+//
+//    @Override
+//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+//        OAuth2User user =  super.loadUser(userRequest);
+//        return new CustomOAuth2User(user);
+//    }
+//}
--- src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
+++ src/main/java/com/example/DoAnJava/Utils/SecurityConfig5.java
@@ -1,38 +1,30 @@
-//package com.example.DoAnJava.Utils;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig5 extends WebSecurityConfigurationAdapter {
-//    @Autowired
-//    private CustomOAuth2UserService oAuth2UserService;
-//
-//    @Autowired
-//    private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/", "/login", "/oauth/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().permitAll()
-//                .and()
-//                .oauth2Login()
-//                .loginPage("/login")
-//                .userInfoEndpoint()
-//                .userService(oauthUserService);
-//    }
-//
-//    @Autowired
-//    private CustomOAuth2UserService oauthUserService;
-//}
+package com.example.DoAnJava.Utils;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig5{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest()
+                                .authenticated()
+                )
+                .oauth2Login();
+
+        return http.build();
+    }
+
+}
