b80db4b3568784a1a01375eb640e18069f614c7e
VuTrungNghia <vunghia467@gmail.com>
2023-08-24 12:42:26 +0700

    update

--- src/main/java/com/example/DoAnJava/controller/CommentController.java
+++ src/main/java/com/example/DoAnJava/controller/CommentController.java
@@ -1,28 +1,52 @@
 package com.example.DoAnJava.controller;
 
 
+import com.example.DoAnJava.entity.Comment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/comment")
+@RequestMapping("/comments")
 public class CommentController {
 
-    private List<String> comments = new ArrayList<>();
+    private List<Comment> comments = new ArrayList<>();
+    private Long nextId = 1L;
 
-    @GetMapping("/comments")
-    public String showComments(Model model) {
+    @GetMapping("/chat")
+    public String showCommentPage(Model model) {
         model.addAttribute("comments", comments);
-        return "home/contact";
+        return "comment/showchat";
     }
 
-    @PostMapping("/comments")
-    public String addComment(@RequestParam String message) {
-        comments.add(message);
-        return "home/index"; // Chuyển hướng để hiển thị danh sách bình luận mới
+    @PostMapping
+    @ResponseBody
+    public Comment addComment(@RequestBody Comment comment) {
+        comment.setId(nextId);
+        comments.add(comment);
+        nextId++;
+        return comment;
     }
+//    @PostMapping("/delete-comment")
+//    @ResponseBody
+//    public ResponseEntity<String> deleteComment(@RequestBody Map<String, Long> request) {
+//        Long id = request.get("id");
+//        Comment commentToRemove = null;
+//        for (Comment comment : comments) {
+//            if (comment.getId().equals(id)) {
+//                commentToRemove = comment;
+//                break;
+//            }
+//        }
+//        if (commentToRemove != null) {
+//            comments.remove(commentToRemove);
+//            return ResponseEntity.ok().build();
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
 }
---
+++ src/main/java/com/example/DoAnJava/entity/Comment.java
@@ -0,0 +1,22 @@
+package com.example.DoAnJava.entity;
+
+public class Comment {
+    private Long id;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
