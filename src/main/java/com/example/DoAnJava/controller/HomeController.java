7d55924c58177a01928d64daab2c5428ce7613e6
VuTrungNghia <vunghia467@gmail.com>
2023-09-22 21:27:03 +0700

    update

--- src/main/java/com/example/DoAnJava/controller/HomeController.java
+++ src/main/java/com/example/DoAnJava/controller/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
     @GetMapping
     public String home(Model model)
     {
-        String url = "http://shoptopping-89b153dfa8dc.herokuapp.com/product/list";
+        String url = "puzzled-gun-production.up.railway.app/product/list";
         List products = this.restTemplate.getForObject(url, List.class);
         model.addAttribute("products",products);
         return  "home/index";
